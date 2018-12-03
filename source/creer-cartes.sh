85b72e0d2caa93690f2ae5c156563333dda4f145
Léon Lenclos <leon.lenclos@gmail.com>
2018-12-03 13:39:23 +0100

    add imprimable zip

--- source/creer-cartes.sh
+++ source/creer-cartes.sh
@@ -10,5 +10,8 @@ montage -density 300 cartes-jeu-pdf/* -geometry +0+0 -tile 4x4  ../imprimable/ca
 echo "montage cartes de parti"
 montage -density 300 cartes-parti-pdf/* -geometry +0+0 -tile 2x2  ../imprimable/cartes-parti.pdf
 
+# Faire l'archivage
+zip ../imprimable/pollos-para-todos-imprimable.zip ../imprimable/LISEZ_MOI.txt ../imprimable/cartes-jeu.pdf ../imprimable/cartes-parti.pdf
+
 # Suprimer les répertoires
 rm -r cartes-jeu-pdf cartes-parti-pdf
